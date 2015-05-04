615d167799369072a57f5e4d85c15a65327f125b
deyingong <gongdeyin@blinnnk.com>
2015-05-04 16:04:47 +0800

    first add project

---
+++ hit56/hit56/class/ViewContrller/BaseViewController.m
@@ -0,0 +1,26 @@
+//
+//  BaseViewController.m
+//  hit56
+//
+//  Created by gongdeyin on 4/26/15.
+//  Copyright (c) 2015 anlu. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface BaseViewController ()
+
+@end
+
+@implementation BaseViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+@end
